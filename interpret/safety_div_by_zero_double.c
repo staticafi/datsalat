4c8d1c0802659b37893ffc638fb5dc209578c1ca
Marek Trtik <trtikm@gmail.com>
2024-03-22 19:14:01 +0100

    Updated interpretation test of floating division

--- interpret/safety_div_by_zero_double.c
+++ interpret/safety_div_by_zero_double.c
@@ -4,7 +4,6 @@ int main()
 {
     double a = __VERIFIER_nondet_double();
     double b = __VERIFIER_nondet_double();
-    if (b == 0) CRASHX(0);
     double c = a / b;
     WRITE(f64, c);
     RET(0);
