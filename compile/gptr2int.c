2c84a73993ff250b057b7f703d9809f294f78f00
Marek Trtik <trtikm@gmail.com>
2025-12-05 13:33:52 +0100

    Added compiler benchmark for global ptr2int

---
+++ compile/gptr2int.c
@@ -0,0 +1,8 @@
+extern void *__crc_w1_ds2760_read __attribute__((__weak__)) ;
+static unsigned long const __kcrctab_w1_ds2760_read __attribute__((__used__, __unused__,
+__section__("___kcrctab+w1_ds2760_read"))) = (unsigned long const )((unsigned long )(& __crc_w1_ds2760_read));
+
+int main(void)
+{
+    return 0;
+}
